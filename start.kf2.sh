7c2c0831f0f62a4d74a643c3b467e87b2f81fd26
char00les <char00les@gmail.com>
2020-09-13 17:41:15 -0400

    Update start.kf2.sh

--- start.kf2.sh
+++ start.kf2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/opt/kf2/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab
+#/opt/kf2/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab
+
+/usr/bin/screen -dmS kf2 /bin/bash -c "/opt/kf2/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab"
